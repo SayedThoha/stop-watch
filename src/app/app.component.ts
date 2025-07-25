8468fff471e41f5a5f9a65740254a34adbf0d1d3
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-07-25 22:19:50 +0530

    basic stop watch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stopWatch';
+
+  ngOnInit(): void {}
+
+  seconds: number = 0;
+  minutes: number = 0;
+  hour: number = 0;
+  interval: any;
+
+  start() {
+    this.interval = setInterval(() => {
+      this.seconds++;
+      if (this.seconds > 9) {
+        this.minutes++;
+        this.seconds = 0;
+      }
+    }, 1000);
+  }
+
+  reset() {
+    this.seconds = 0;
+    this.minutes = 0;
+    clearInterval(this.interval);
+  }
 }
